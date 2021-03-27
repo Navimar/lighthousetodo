7382a351a7355bc08e20877b016fb199155cc049
Navimar <immcreator@gmail.com>
2021-03-27 19:29:53 +0300

    data out

--- server/run.js
+++ server/run.js
@@ -31,13 +31,13 @@ module.exports = (io) => {
             });
 
             let dir = sha256(sha256.x2(msg.id + salt));
-            if (!fs.existsSync('./data/' + dir)) {
+            if (!fs.existsSync('../data/' + dir)) {
                 console.log('folder created');
-                fs.mkdirSync('./data/' + dir);
+                fs.mkdirSync('../data/' + dir);
             }
 
-            fs.createReadStream('data/' + dir + '/data.txt').pipe(fs.createWriteStream('data/' + dir + '/old' + n + '.txt'));
-            fs.writeFileSync('data/' + dir + '/data.txt', JSON.stringify(msg), function (err) {
+            fs.createReadStream('../data/' + dir + '/data.txt').pipe(fs.createWriteStream('..data/' + dir + '/old' + n + '.txt'));
+            fs.writeFileSync('../data/' + dir + '/data.txt', JSON.stringify(msg), function (err) {
                 return console.log(err);
             });
             n++;
