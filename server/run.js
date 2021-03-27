ccc28fa60327a0798ce8116078ab1a0f3da8cb1f
Navimar <immcreator@gmail.com>
2021-03-27 17:37:50 +0300

    async write

--- server/run.js
+++ server/run.js
@@ -16,32 +16,32 @@ module.exports = (io) => {
         });
 
         socket.on('save', function (msg) {
-            //     if (!msg.id) {
-            //         msg.id = 'demo';
-            //     }
-            //     user.add(msg.id, socket);
+            if (!msg.id) {
+                msg.id = 'demo';
+            }
+            user.add(msg.id, socket);
 
-            //     // let sockets = user.get(msg.id)
-            //     // // console.log(sockets);
-            //     // sockets.forEach(s => {
-            //     //     if (s != socket) {
-            //     //         // load(msg.id, s);
-            //     //         s.disconnect(true)
-            //     //     }
-            //     // });
+            let sockets = user.get(msg.id)
+            // console.log(sockets);
+            sockets.forEach(s => {
+                if (s != socket) {
+                    // load(msg.id, s);
+                    s.disconnect(true)
+                }
+            });
 
-            //     let dir = sha256(sha256.x2(msg.id + salt));
-            //     if (!fs.existsSync('./data/' + dir)) {
-            //         console.log('folder created');
-            //         fs.mkdirSync('./data/' + dir);
-            //     }
+            let dir = sha256(sha256.x2(msg.id + salt));
+            if (!fs.existsSync('./data/' + dir)) {
+                console.log('folder created');
+                fs.mkdirSync('./data/' + dir);
+            }
 
-            //     fs.createReadStream('data/' + dir + '/data.txt').pipe(fs.createWriteStream('data/' + dir + '/old' + n + '.txt'));
-            //     fs.writeFileSync('data/' + dir + '/data.txt', JSON.stringify(msg), function (err) {
-            //         return console.log(err);
-            //     });
-            //     n++;
-            //     if (n > 100) { n = 0 }
+            fs.createReadStream('data/' + dir + '/data.txt').pipe(fs.createWriteStream('data/' + dir + '/old' + n + '.txt'));
+            fs.writeFile('data/' + dir + '/data.txt', JSON.stringify(msg), function (err) {
+                return console.log(err);
+            });
+            n++;
+            if (n > 100) { n = 0 }
 
         });
         socket.on('load', function (msg) {
