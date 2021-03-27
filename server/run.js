e4f636dd59a482f13b93b3be8bb47c2af3c0cfb0
Navimar <immcreator@gmail.com>
2021-03-27 19:01:57 +0300

    a

--- server/run.js
+++ server/run.js
@@ -35,11 +35,11 @@ module.exports = (io) => {
                 console.log('folder created');
                 fs.mkdirSync('./data/' + dir);
             }
-            JSON.stringify(msg);
+
             // fs.createReadStream('data/' + dir + '/data.txt').pipe(fs.createWriteStream('data/' + dir + '/old' + n + '.txt'));
-            // fs.writeFile('data/' + dir + '/data.txt', JSON.stringify(msg), function (err) {
-            //     return console.log(err);
-            // });
+            fs.writeFile('data/' + dir + '/data.txt', JSON.stringify(msg), function (err) {
+                return console.log(err);
+            });
             n++;
             if (n > 100) { n = 0 }
 
