16be65e910b4d43f0d315a8d90d43261edf91702
Navimar <immcreator@gmail.com>
2021-11-18 11:07:30 +0300

    weight sort

--- static/src/sort.js
+++ static/src/sort.js
@@ -48,12 +48,6 @@ let sortdata = () => {
       else if (a.tags.length < b.tags.length) {
         return 1
       }
-      if (a.tags[0] && b.tags[0] && (a.tags[0].localeCompare(b.tags[0]) < 0)) {
-        return -1
-      }
-      else if (a.tags[0] && b.tags[0] && (a.tags[0].localeCompare(b.tags[0]) > 0)) {
-        return 1
-      }
       if (a.weight > b.weight)
         return -1
       else if (a.weight < b.weight)
@@ -62,6 +56,12 @@ let sortdata = () => {
         return -1
       }
       else if (a.opns.length < b.opns.length) {
+        return 1
+      }
+      if (a.tags[0] && b.tags[0] && (a.tags[0].localeCompare(b.tags[0]) < 0)) {
+        return -1
+      }
+      else if (a.tags[0] && b.tags[0] && (a.tags[0].localeCompare(b.tags[0]) > 0)) {
         return 1
       }
       if (isReady(a.date, a.time) < isReady(b.date, b.time))
