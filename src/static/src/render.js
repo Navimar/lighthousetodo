45162470aaa70d948d791ffdf0000c7b6797e937
Navimar <immcreator@gmail.com>
2023-02-07 23:31:24 +0400

    wothout type

--- src/static/src/render.js
+++ src/static/src/render.js
@@ -173,8 +173,8 @@ let render = () => {
 
         texthtml += "<div class='header'>Приоритет <input  class='checkboxdip onoff' type=\"checkbox\"></label></div>"
 
-        texthtml += "<input type=\"number\" class='dateinp profitinp' id=\"dip\" name=\"profitinp\">";
-
+        texthtml += "<input inputmode='decimal' class='dateinp profitinp' id=\"dip\" name=\"profitinp\">";
+        //type=\"number\"
         texthtml += "</div>";
 
         // texthtml += "<span class='header'>+</span>"
