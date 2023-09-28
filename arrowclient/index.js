64c2aad86d0849d7c7f0e2f25714e0236dbbab8e
Navimar <immcreator@gmail.com>
2023-09-28 07:30:06 +0400

    css: pan-y for body

--- arrowclient/index.js
+++ arrowclient/index.js
@@ -88,10 +88,6 @@ const render = html`
 `
 
 window.addEventListener("load", function () {
-  document.addEventListener("gesturestart", function (e) {
-    e.preventDefault()
-  })
-
   window.addEventListener("paste", function (e) {
     e.preventDefault()
     let text = e.clipboardData.getData("text/plain")
