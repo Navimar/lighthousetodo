1567b551c0e44f0643e6718af57119da01760911
Navimar <immcreator@gmail.com>
2023-09-29 11:14:25 +0400

    webkit-overflow-scrolling

--- arrowclient/index.js
+++ arrowclient/index.js
@@ -79,7 +79,6 @@ function updateCurrentTimeMarker() {
 //       </button>
 //     </div>
 const render = html`
-  <div class="bgimg bg-nearwhite dark:bg-black fixed w-full h-full -z-10 bg-cover"></div>
   <div class="flex flex-col gap-4 pb-80 max-w-full w-40rem m-auto">
     ${() => search()} ${() => authentication()} ${() => renderCalendar(dayjs())} ${() => renderTasks()}
   </div>
