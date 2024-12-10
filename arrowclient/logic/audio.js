09e1f9bf2dae0c45528a0d340ecd5fc4b4519aee
Navimar <immcreator@gmail.com>
2024-12-10 09:16:00 +0400

    silence

--- arrowclient/logic/audio.js
+++ arrowclient/logic/audio.js
@@ -2,36 +2,36 @@ import { Howl } from "howler";
 
 // Предварительная загрузка звуков
 const sounds = {
-  add: new Howl({
-    src: ["../add.wav"],
-    loop: false,
-    volume: 0.5,
-  }),
-  readySave: new Howl({
-    src: ["../readySave.wav"],
-    loop: false,
-    volume: 0.4,
-  }),
-  save: new Howl({
-    src: ["../save.wav"],
-    loop: false,
-    volume: 0.5,
-  }),
-  ready: new Howl({
-    src: ["../ready.wav"],
-    loop: false,
-    volume: 0.5,
-  }),
-  unready: new Howl({
-    src: ["../unready.wav"],
-    loop: false,
-    volume: 0.5,
-  }),
-  afterward: new Howl({
-    src: ["../afterward.wav"],
-    loop: false,
-    volume: 0.5,
-  }),
+  // add: new Howl({
+  //   src: ["../add.wav"],
+  //   loop: false,
+  //   volume: 0.5,
+  // }),
+  // readySave: new Howl({
+  //   src: ["../readySave.wav"],
+  //   loop: false,
+  //   volume: 0.4,
+  // }),
+  // save: new Howl({
+  //   src: ["../save.wav"],
+  //   loop: false,
+  //   volume: 0.5,
+  // }),
+  // ready: new Howl({
+  //   src: ["../ready.wav"],
+  //   loop: false,
+  //   volume: 0.5,
+  // }),
+  // unready: new Howl({
+  //   src: ["../unready.wav"],
+  //   loop: false,
+  //   volume: 0.5,
+  // }),
+  // afterward: new Howl({
+  //   src: ["../afterward.wav"],
+  //   loop: false,
+  //   volume: 0.5,
+  // }),
 };
 
 // Функция для генерации случайного числа в заданном диапазоне
@@ -42,20 +42,22 @@ function randomFloat(min, max) {
 // Экспортируемый объект с методами для управления звуками
 export default {
   playSound: (soundName) => {
-    const sound = sounds[soundName];
-    if (!sound) {
-      console.error(`Sound "${soundName}" not found`);
-      return;
-    }
+    // const sound = sounds[soundName];
+    // if (!sound) {
+    //   console.error(`Sound "${soundName}" not found`);
+    //   return;
+    // }
 
-    // Генерация случайных значений для параметров
-    const rate = randomFloat(0.8, 1.2); // Скорость воспроизведения от 0.8 до 1.2
-    const volume = randomFloat(0.8, 1.0); // Громкость от 0.8 до 1.0
+    // // Генерация случайных значений для параметров
+    // const rate = randomFloat(0.8, 1.2); // Скорость воспроизведения от 0.8 до 1.2
+    // const volume = randomFloat(0.8, 1.0); // Громкость от 0.8 до 1.0
 
-    // Применение параметров и воспроизведение звука
-    sound.rate(rate);
-    sound.volume(volume);
-    sound.play();
-  },
-  sounds,
+    // // Применение параметров и воспроизведение звука
+    // sound.rate(rate);
+    // sound.volume(volume);
+    // sound.play();
+    // console.log(Object.keys(Howler._howls).length);
+    // console.log(Howler.ctx);
+
+  }
 };
--- server/version.js
+++ server/version.js
@@ -1,1 +1,1 @@
-export default "0.6.7"
+export default "0.6.8"
