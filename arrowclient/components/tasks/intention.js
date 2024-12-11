1c319a7ef2a0319ee0a23fd29ec6307fbeb13c45
Navimar <immcreator@gmail.com>
2024-12-11 08:48:29 +0400

    intentionPriority

--- arrowclient/components/tasks/intention.js
+++ arrowclient/components/tasks/intention.js
@@ -73,7 +73,8 @@ function renderIntention(task, index, handleDragStart, handleDragEnd, handleDrop
     class="flex flex-col gap-3 break-words bg-neutral-100 dark:bg-neutral-900 p-3 rounded-lg overflow dark:text-white"
     ><div class="ml-2 flex flex-wrap-reverse gap-3"
       ><div class="w-[28rem] max-w-full mr-auto "
-        >${() => task.name}${() => {
+        >${() => task.intentionPriority}
+        ${() => task.name}${() => {
           if (task.note && task.note.length > 0) return " 📝"
         }}</div
       ><div class="flex gap-3">${() => taskPlate(task, "p-1")}</div></div
