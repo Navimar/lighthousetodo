611b1e5ec8da81d77a4f8053ebf107641340575a
Navimar <immcreator@gmail.com>
2025-03-05 17:46:27 +0400

    sandclock plate

--- arrowclient/components/tasks/taskplate.js
+++ arrowclient/components/tasks/taskplate.js
@@ -4,66 +4,20 @@ import dayjs from "dayjs"
 export default (task, additionalClass = "") => {
   let taskDate = dayjs(`${task.date}T${task.time}`, "YYYY-MM-DDTHH:mm")
 
-  let getTaskTime = () => {
-    return ""
-    if (task.ready) return "готово"
-    if (task.urgency == "onTime") return task.time
-
-    let now = dayjs()
-    if (taskDate.isAfter(now.endOf("day"))) return `с ${taskDate.format("DD.MM")}`
-    if (taskDate.isAfter(now)) return `с ${taskDate.format("HH:mm")}`
-    if (task.intention) return "цель"
-    if (task.urgency == "onDay" && taskDate.isBefore(now.startOf("day").subtract(1, "day"))) {
-      return "давно"
-    } else if (task.urgency == "onDay" && taskDate.isBefore(now.startOf("day"))) {
-      return "вчера"
-    } else if (task.urgency == "onDay" && taskDate.isSame(now, "day")) {
-      return "сегодня"
-    } else if (task.urgency == "onDay") {
-      return dayjs(task.date).format("DD.MM")
-    } else if (task.urgency == "shortTerm") {
-      return "на днях"
-    } else return ""
-  }
-
-  let paused = () => {
-    return task.pause ? "II" : ""
-  }
-
-  const bulletClass = () => {
-    if (task.ready) return "text-black"
-    if (task.intention) return "text-compliment"
-    if (task.importance == "critical") return "text-accent"
-    if (task.importance == "important") return "text-yellow-500"
-    if (task.importance == "noticeable") return "text-lime-500"
-    if (task.importance == "trivial") return "text-neutral-350 dark:text-neutral-600"
-    return ""
-  }
-
   const bulletSymbol = () => {
     let symbol = ""
-    if (task.ready) symbol = "<b class='text-accent text-base'>✓</b>"
-    else if (task.intention) symbol = "<b class='text-base'>!</b>"
-    else
-      switch (task.difficulty) {
-        case "long":
-          symbol = "▲"
-          break
-        case "day":
-          symbol = "◉"
-          break
-        case "hour":
-          symbol = "<span class='text-base'>⧗</span>"
-          break
-        case "quick":
-          symbol = "★"
-          break
-        case "kairos":
-          return ""
-        default:
-          symbol = "?"
+    let now = dayjs()
+    if (task.pause) {
+      symbol = "<b class='text-base'>II</b>" // Пауза как приоритетный символ
+    } else if (task.ready) {
+      symbol = "<b class='text-accent text-base'>✓</b>"
+    } else {
+      // Проверяем, находится ли задача в будущем
+      if (taskDate.isAfter(now)) {
+        symbol = "<span class='text-yellow-500 text-base'>⧗</span>" // Песочные часы для задач в будущем
       }
-    return `<span class="${bulletClass()} text-xs inline-block">${symbol}</span>`
+    }
+    return `<span class="text-xs inline-block">${symbol}</span>`
   }
 
   const isPastTask = () => {
@@ -74,10 +28,37 @@ export default (task, additionalClass = "") => {
   let finalClass = `${additionalClass} ${isPastTask() ? "text-accent" : ""}`.trim()
 
   return html`<div
-      class="h-fit box-border font-bold text-center uppercase whitespace-nowrap fontaccent text-sm empty:hidden ${finalClass}"
-      >${paused()}</div
-    ><div
-      class="h-fit flex items-center text-center uppercase whitespace-nowrap fontaccent text-sm gap-2 empty:hidden ${finalClass}"
-      >${bulletSymbol()}${getTaskTime()}</div
-    >`
+    class="h-fit flex items-center text-center uppercase whitespace-nowrap fontaccent text-sm gap-2 empty:hidden ${finalClass}"
+    >${bulletSymbol()}</div
+  >`
 }
+
+// else if (task.intention) symbol = "<b class='text-base'>!</b>"
+// switch (task.difficulty) {
+//   case "long":
+//     symbol = "▲"
+//     break
+//   case "day":
+//     symbol = "◉"
+//     break
+//   case "hour":
+//     symbol = "<span class='text-yellow-500 text-base'>⧗</span>"
+//     break
+//   case "quick":
+//     symbol = "★"
+//     break
+//   case "kairos":
+//     return ""
+//   default:
+//     symbol = "?"
+// }
+
+// const bulletClass = () => {
+//   if (task.ready) return "text-black"
+//   if (task.intention) return "text-compliment"
+//   if (task.importance == "critical") return "text-accent"
+//   if (task.importance == "important") return "text-yellow-500"
+//   if (task.importance == "noticeable") return "text-lime-500"
+//   if (task.importance == "trivial") return "text-neutral-350 dark:text-neutral-600"
+//   return ""
+// }
