4baccedad9fcbfacf58b4ad638c72a26e77ef3e4
Navimar <immcreator@gmail.com>
2025-08-15 20:45:25 +0400

    hidden

--- arrowclient/components/tasks/hiddenData.js
+++ arrowclient/components/tasks/hiddenData.js
@@ -2,6 +2,6 @@ import { html } from "~/arrow-js/index.js"
 
 // Отображает скрытые данные задачи
 export default (task) => {
-  return html``
-  // return html` <div class="text-[0.6rem] italic text-gray-500"> Depth: ${task.depth} </div> `
+  // return html``
+  return html` <div class="text-[0.6rem] italic text-gray-500"> Depth: ${task.depth} </div> `
 }
